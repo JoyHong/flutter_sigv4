3ec6dad07cf56024d5cf82116c680e086b737071
joy.chen <joy.chen@justalk.com>
2024-05-09 14:00:53 +0800

    refactor: dio 扩展签名新增时间戳参数

--- lib/src/extensions/dio.dart
+++ lib/src/extensions/dio.dart
@@ -4,13 +4,14 @@ import 'package:dio/dio.dart';
 import 'package:sigv4/sigv4.dart';
 
 extension DioExtension on RequestOptions {
-  RequestOptions sign(Sigv4Client client) {
+  RequestOptions sign(Sigv4Client client, {String? dateTime}) {
     final signed = client.signedHeaders(
       client.canonicalUrl('$baseUrl$path', query: queryParameters),
       method: method,
       query: queryParameters,
       headers: headers,
       body: data != null ? jsonEncode(data) : null,
+      dateTime: dateTime
     );
 
     headers.addAll(signed);
